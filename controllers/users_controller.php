c4992327e52a27fc714e446e1bcda7cf3da9a651
Emmanuel <emmanuelvillada1903@gmail.com>
2024-05-06 21:56:57 -0500

    finishign the user controller

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -23,10 +23,38 @@ class user_controller
     }
     public function update(User $user)
     {
-        $pdo = $this->db_connection->pdo;
-        $query = "UPDATE user SET username =?, password =?, identity =? WHERE user_id =?";
-        $stmt = $pdo->prepare($query);
-        $stmt->execute(array($user->username, $user->password, $user->identity, $user->user_id));
-        return $pdo->lastInsertId();
+        try {
+            $pdo = $this->db_connection->pdo;
+            $query = "UPDATE user SET username =?, password =?, identity =? WHERE user_id =?";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute(array($user->username, $user->password, $user->identity, $user->user_id));
+            return $pdo->lastInsertId();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+    public function delete(User $user)
+    {
+        try {
+            $pdo = $this->db_connection->pdo;
+            $query = "DELETE FROM user WHERE user_id =?";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute(array($user->user_id));
+            return $pdo->lastInsertId();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+    public function read()
+    {
+        try {
+            $pdo = $this->db_connection->pdo;
+            $query = "SELECT * FROM user";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
 }
